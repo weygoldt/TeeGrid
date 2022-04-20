19a4936c2eb975152cce31b85977bbfdf795c0a8
Jan Benda <jan.benda@uni-tuebingen.de>
2022-04-20 10:33:44 +0200

    improved error handling of file issues

--- 8channel-logger/8channel-logger.ino
+++ 8channel-logger/8channel-logger.ino
@@ -27,7 +27,7 @@ char path[] = "recordings";              // folder where to store the recordings
 char fileName[] = "grid1-SDATETIME.wav"; // may include DATE, SDATE, TIME, STIME, DATETIME, SDATETIME, ANUM, NUM
 float fileSaveTime = 10*60;              // seconds
 
-float initialDelay = 10.0;               // seconds
+float initialDelay = 1.0;                // seconds
 
 int pulseFrequency = 230;                // Hertz
 int signalPins[] = {9, 8, 7, 6, 5, 4, 3, 2, -1}; // pins where to put out test signals
@@ -81,7 +81,7 @@ String makeFileName() {
   }
   name = file.incrementFileName(name);
   if (name.length() == 0) {
-    Serial.println("WARNING: failed to open file on SD card.");
+    Serial.println("WARNING: failed to increment file name.");
     Serial.println("SD card probably not inserted.");
     Serial.println();
     return "";
@@ -97,20 +97,19 @@ bool openNextFile(const String &name) {
   String fname = name + ".wav";
   char dts[20];
   rtclock.dateTime(dts);
-  file.openWave(fname.c_str(), -1, dts);
-  file.write();
-  Serial.println(fname);
-  if (file.isOpen()) {
-    blink.setSingle();
-    blink.blinkSingle(0, 1000);
-    return true;
-  }
-  else {
+  if (! file.openWave(fname.c_str(), -1, dts)) {
     Serial.println();
     Serial.println("WARNING: failed to open file on SD card.");
-    Serial.println("SD card probably not inserted.");
+    Serial.println("SD card probably not inserted or full -> halt");
+    aidata.stop();
+    while (1) {};
     return false;
   }
+  file.write();
+  Serial.println(fname);
+  blink.setSingle();
+  blink.blinkSingle(0, 1000);
+  return true;
 }
 
 
@@ -135,41 +134,50 @@ void storeData() {
       Serial.println();
       Serial.println("ERROR in writing data to file:");
       switch (samples) {
-        case -1: Serial.println("  File not open."); break;
-        case -2: Serial.println("  No data available, data acquisition probably not running."); break;
-        case -3: Serial.println("  File already full."); break;
-        case 0: Serial.println("  Nothing written into the file."); break;
-        default: Serial.printf("  Unknown error %d.\n", samples);
+        case 0:
+          Serial.println("  Nothing written into the file.");
+          Serial.println("  SD card probably full -> halt");
+          aidata.stop();
+          while (1) {};
+          break;
+        case -1:
+          Serial.println("  File not open.");
+          break;
+        case -2:
+          Serial.println("  File already full.");
+          break;
+        case -3:
+          Serial.println("  No data available, data acquisition probably not running.");
+          Serial.println("  sampling rate probably too high,");
+          Serial.println("  given the number of channels, averaging, sampling and conversion speed.");
+          break;
       }
-      Serial.println("  sampling rate probably too high,");
-      Serial.println("  given the number of channels, averaging, sampling and conversion speed.");
-      if (samples != -3) {
+      if (samples == -3) {
         aidata.stop();
-        file.close();
+        file.closeWave();
         sensors.closeCSV();
         char mfs[20];
         sprintf(mfs, "error%d-%d.msg", restarts+1, -samples);
         FsFile mf = sdcard.openWrite(mfs);
         mf.close();
-        if (samples == 0)
-          samples = -1;
       }
-      else
-        samples = 0;
     }
     if (file.endWrite() || samples < 0) {
       file.close();  // file size was set by openWave()
       String name = makeFileName();
       if (samples < 0) {
+        restarts++;
         if (restarts >= 5) {
           Serial.println("ERROR: Too many file errors -> halt.");
+          aidata.stop();
           while (1) {};
         }
+      }
+      if (samples == -3) {
         String sname = name + "-temperatures";
         sensors.openCSV(sdcard, sname.c_str());
         aidata.start();
         file.start();
-        restarts++;
       }
       openNextFile(name);
     }
@@ -208,12 +216,16 @@ void setup() {
   else
     delay(uint32_t(1000.0*settings.InitialDelay));
   String name = makeFileName();
+  if (name.length() == 0) {
+    Serial.println("-> halt");
+    aidata.stop();
+    while (1) {};
+  }
   String sname = name + "-temperatures";
   sensors.openCSV(sdcard, sname.c_str());
   sensors.start();
   file.start();
   openNextFile(name);
-  delay(5);
 }
 
 
