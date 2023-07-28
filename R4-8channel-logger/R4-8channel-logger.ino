9a3a99faf5268c3e4b3757235460e6daf53d4fe8
Jan Benda <jan.benda@uni-tuebingen.de>
2023-07-28 18:46:38 +0200

    [R4-8channel-logger] still not working

--- R4-8channel-logger/R4-8channel-logger.ino
+++ R4-8channel-logger/R4-8channel-logger.ino
@@ -1,4 +1,4 @@
-#define SINGLE_FILE_MTP
+//#define SINGLE_FILE_MTP
 
 #include <Wire.h>
 #include <ControlPCM186x.h>
@@ -15,22 +15,21 @@
 // Default settings: ----------------------------------------------------------
 // (may be overwritten by config file teegrid.cfg)
 #define SAMPLING_RATE 48000  // samples per second and channel in Hertz
-#define GAIN 20.0            // dB
+#define GAIN 0.0            // dB
 
 #define PATH          "recordings"      // folder where to store the recordings
-#define FILENAME      "grid1-SDATETIME" // may include DATE, SDATE, TIME, STIME, DATETIME, SDATETIME, ANUM, NUM
-#define FILE_SAVE_TIME 10    // seconds
+//#define FILENAME      "grid1-SDATETIME" // may include DATE, SDATE, TIME, STIME, DATETIME, SDATETIME, ANUM, NUM
+#define FILENAME      "RECORDING" // may include DATE, SDATE, TIME, STIME, DATETIME, SDATETIME, ANUM, NUM
+#define FILE_SAVE_TIME 10.0  // seconds
 #define INITIAL_DELAY  2.0   // seconds
 
 // ----------------------------------------------------------------------------
 
 #define VERSION        "1.0"
 
-RTClock rtclock;
-
 DATA_BUFFER(AIBuffer, NAIBuffer, 512*256)
-ControlPCM186x pcm1;
 TeensyTDM aidata(AIBuffer, NAIBuffer);
+ControlPCM186x pcm1(PCM186x_I2C_ADDR1);
 
 SDCard sdcard;
 SDWriter file(sdcard, aidata);
@@ -38,6 +37,7 @@ SDWriter file(sdcard, aidata);
 Configurator config;
 Settings settings(PATH, FILENAME, FILE_SAVE_TIME, 100.0,
                   0.0, INITIAL_DELAY);
+RTClock rtclock;
 String prevname; // previous file name
 Blink blink(LED_BUILTIN);
 
@@ -62,10 +62,15 @@ String makeFileName() {
 }
 
 
-bool openNextFile(const String &name) {
+void openNextFile(const String &name) {
   blink.clear();
-  if (name.length() == 0)
-    return false;
+  if (name.length() == 0) {
+    Serial.println();
+    Serial.println("WARNING: no file name.");
+    aidata.stop();
+    while (1) { yield(); };
+    return;
+  }
   String fname = name + ".wav";
   char dts[20];
   rtclock.dateTime(dts);
@@ -74,14 +79,13 @@ bool openNextFile(const String &name) {
     Serial.println("WARNING: failed to open file on SD card.");
     Serial.println("SD card probably not inserted or full -> halt");
     aidata.stop();
-    while (1) {};
-    return false;
+    while (1) { yield(); };
+    return;
   }
   file.write();
   Serial.println(fname);
   blink.setSingle();
   blink.blinkSingle(0, 1000);
-  return true;
 }
 
 
@@ -93,7 +97,7 @@ void setupStorage() {
     Serial.printf("Save recorded data in folder \"%s\".\n\n", settings.Path);
   file.setWriteInterval();
   file.setMaxFileTime(settings.FileTime);
-  char ss[30] = "TeeGrid R4-8channel-logger v";
+  char ss[40] = "TeeGrid R4-8channel-logger v";
   strcat(ss, VERSION);
   file.header().setSoftware(ss);
 }
@@ -121,14 +125,12 @@ void storeData() {
           break;
         case -3:
           Serial.println("  No data available, data acquisition probably not running.");
-          Serial.println("  sampling rate probably too high,");
-          Serial.println("  given the number of channels, averaging, sampling and conversion speed.");
           break;
       }
       if (samples == -3) {
         aidata.stop();
         file.closeWave();
-        char mfs[20];
+        char mfs[30];
         sprintf(mfs, "error%d-%d.msg", restarts+1, -samples);
         File mf = sdcard.openWrite(mfs);
         mf.close();
@@ -137,11 +139,12 @@ void storeData() {
     if (file.endWrite() || samples < 0) {
       file.close();  // file size was set by openWave()
 #ifdef SINGLE_FILE_MTP
+      blink.clear();
       MTP.begin();
-      MTP.addFilesystem(SD, "Grid");
+      MTP.addFilesystem(sdcard, "Grid");
       while (true) {
         MTP.loop();
-	yield();
+	      yield();
       }
 #endif      
       String name = makeFileName();
@@ -150,7 +153,7 @@ void storeData() {
         if (restarts >= 5) {
           Serial.println("ERROR: Too many file errors -> halt.");
           aidata.stop();
-          while (1) {};
+          while (1) { yield(); };
         }
       }
       if (samples == -3) {
@@ -180,7 +183,8 @@ void setup() {
   Wire.begin();
   pcm1.begin();
   pcm1.setMicBias(false, true);
-  pcm1.setupTDM(ControlPCM186x::CH1L, ControlPCM186x::CH1R, ControlPCM186x::CH2L, ControlPCM186x::CH2R, false);
+  //pcm1.setupTDM(ControlPCM186x::CH1L, ControlPCM186x::CH1R, ControlPCM186x::CH2L, ControlPCM186x::CH2R, false);
+  pcm1.setupTDM(ControlPCM186x::CH3L, ControlPCM186x::CH3R, ControlPCM186x::CH4L, ControlPCM186x::CH4R, false);
   pcm1.setGain(ControlPCM186x::ADCLR, GAIN);
   pcm1.setFilters(ControlPCM186x::FIR, false);
   char ws[30];
@@ -204,11 +208,6 @@ void setup() {
   else
     delay(uint32_t(1000.0*settings.InitialDelay));
   String name = makeFileName();
-  if (name.length() == 0) {
-    Serial.println("-> halt");
-    aidata.stop();
-    while (1) {};
-  }
   file.start();
   openNextFile(name);
 }
