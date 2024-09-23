5546d2d30a54c4dc12788639cc44944de9df78ca
Jan Benda <jan.benda@uni-tuebingen.de>
2024-09-23 17:51:26 +0200

    [R4-sensors-logger] fix conflict leftover, updated menu

--- examples/R4-sensors-logger/R4-sensors-logger.ino
+++ examples/R4-sensors-logger/R4-sensors-logger.ino
@@ -73,16 +73,18 @@ LoadConfigAction load_act(config_menu, "Load configuration", sdcard);
 RemoveConfigAction remove_act(config_menu, "Erase configuration", sdcard);
 Configurable sdcard_menu("SD card", Action::StreamInput);
 SDInfoAction sdinfo_act(sdcard_menu, "SD card info", sdcard);
+SDCheckAction sdcheck_act(sdcard_menu, "SD card check", sdcard);
+SDBenchmarkAction sdbench_act(sdcard_menu, "SD card benchmark", sdcard);
 SDFormatAction format_act(sdcard_menu, "Format SD card", sdcard);
 SDEraseFormatAction eraseformat_act(sdcard_menu, "Erase and format SD card", sdcard);
-SDListAction list_act(sdcard_menu, "List all recordings", sdcard, settings);
-SDRemoveAction erase_act(sdcard_menu, "Erase all recordings", sdcard, settings);
+SDListRootAction listroot_act(sdcard_menu, "List files in root directory", sdcard);
+SDListRecordingsAction listrecs_act(sdcard_menu, "List all recordings", sdcard, settings);
+SDRemoveRecordingsAction eraserecs_act(sdcard_menu, "Erase all recordings", sdcard, settings);
 #ifdef FIRMWARE_UPDATE
 Configurable firmware_menu("Firmware", Action::StreamInput);
 ListFirmwareAction listfirmware_act(firmware_menu, "List available updates", sdcard);
 UpdateFirmwareAction updatefirmware_act(firmware_menu, "Update firmware", sdcard);
 #endif
->>>>>>> e2265719379a9a65fd4458f8ab6bda0b36d79986
 
 ESensors sensors;
 
